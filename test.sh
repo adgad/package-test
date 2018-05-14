07167cd0c50a40046357fe113d8acc9666498af4
Arjun Gadhia <arjungadhia@gmail.com>
2018-05-14 17:12:08 +0100

    Move the shrinkwrap every time

--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@ rm -rf node_modules
 rm -rf package-lock.json
 npm install --no-package-lock
 npm shrinkwrap
-cp npm-shrinkwrap.json ../results/1.json
+mv npm-shrinkwrap.json ../results/1.json
 
 
 ##### No Package-lock.json ######
@@ -15,7 +15,7 @@ npm version patch
 npm publish --access public
 cd ../my-app
 npm install --no-package-lock
-npm shrinkwrap && cp npm-shrinkwrap.json ../results/2.json
+npm shrinkwrap && mv npm-shrinkwrap.json ../results/2.json
 
 
 #Update patch version of n-express. npm update.
@@ -24,7 +24,7 @@ npm version patch
 npm publish --access public
 cd ../my-app
 npm update --no-package-lock
-npm shrinkwrap && cp npm-shrinkwrap.json ../results/3.json
+npm shrinkwrap && mv npm-shrinkwrap.json ../results/3.json
 
 
 #Update patch version of n-express. npm ci.
@@ -33,7 +33,7 @@ npm version patch
 npm publish --access public
 cd ../my-app
 npm ci --no-package-lock
-npm shrinkwrap && cp npm-shrinkwrap.json ../results/4.json
+npm shrinkwrap && mv npm-shrinkwrap.json ../results/4.json
 
 #Update minor version of n-ui. npm install.
 cd ../n-ui
@@ -41,7 +41,7 @@ npm version minor
 npm publish --access public
 cd ../my-app
 npm install --no-package-lock
-npm shrinkwrap && cp npm-shrinkwrap.json ../results/5.json
+npm shrinkwrap && mv npm-shrinkwrap.json ../results/5.json
 
 #Update minor version of n-ui. npm update.
 cd ../n-ui
@@ -49,7 +49,7 @@ npm version minor
 npm publish --access public
 cd ../my-app
 npm update --no-package-lock
-npm shrinkwrap && cp npm-shrinkwrap.json ../results/6.json
+npm shrinkwrap && mv npm-shrinkwrap.json ../results/6.json
 
 #Update minor version of n-ui. npm ci.
 cd ../n-ui
@@ -57,7 +57,7 @@ npm version minor
 npm publish --access public
 cd ../my-app
 npm ci --no-package-lock
-npm shrinkwrap && cp npm-shrinkwrap.json ../results/7.json
+npm shrinkwrap && mv npm-shrinkwrap.json ../results/7.json
 
 #Update minor version of third-party-dep. Update in package.json. npm install.
 cd ../third-party-dep
@@ -66,7 +66,7 @@ npm publish --access public
 cd ../my-app
 perl -i -pe 's/(third-party-dep\": \"\^\d+\.\d+\.)(\d+)/$1.($2 + 1)/e' package.json
 npm install --no-package-lock
-npm shrinkwrap && cp npm-shrinkwrap.json ../results/8.json
+npm shrinkwrap && mv npm-shrinkwrap.json ../results/8.json
 
 #Update minor version of third-party-dep. Update in package.json. npm update.
 cd ../third-party-dep
@@ -75,7 +75,7 @@ npm publish --access public
 cd ../my-app
 perl -i -pe 's/(third-party-dep\": \"\^\d+\.\d+\.)(\d+)/$1.($2 + 1)/e' package.json
 npm update --no-package-lock
-npm shrinkwrap && cp npm-shrinkwrap.json ../results/9.json
+npm shrinkwrap && mv npm-shrinkwrap.json ../results/9.json
 
 #Update minor version of third-party-dep. Update in package.json. npm ci.
 cd ../third-party-dep
@@ -84,5 +84,5 @@ npm publish --access public
 cd ../my-app
 perl -i -pe 's/(third-party-dep\": \"\^\d+\.\d+\.)(\d+)/$1.($2 + 1)/e' package.json
 npm ci --no-package-lock
-npm shrinkwrap && cp npm-shrinkwrap.json ../results/10.json
+npm shrinkwrap && mv npm-shrinkwrap.json ../results/10.json
 
